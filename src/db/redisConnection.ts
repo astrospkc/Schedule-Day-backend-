83aa7a23b9b223a18cff2c13a9a82eb58a7ea656
Punam <punamkumari399@gmail.com>
2025-10-11 17:23:16 +0530

    aiven sevice url connection

--- src/db/redisConnection.ts
+++ src/db/redisConnection.ts
@@ -1,9 +1,26 @@
 import { Redis } from 'ioredis';
+import dotenv from 'dotenv';
 
-const redisConnection = new Redis({
-    host: "127.0.0.1", // or your Redis host
-    port: 6379,        // default Redis port
-    maxRetriesPerRequest: null
+const env = process.env.NODE_ENV || 'local'
+
+dotenv.config({ path: `.env.${env}` });
+
+// Use REDIS_URI from environment variables or fallback to default localhost
+const redisUri = process.env.AIVEN_SERVICE_URI || 'redis://localhost:6379';
+
+const redisConnection = new Redis(redisUri, {
+    maxRetriesPerRequest: null,
+    enableReadyCheck: false, // Disable ready check if you're using Redis Sentinel or Cluster
+    // Add other options as needed
+});
+
+// Optional: Add error handling
+redisConnection.on('error', (err) => {
+    console.error('Redis error:', err);
+});
+
+redisConnection.on('connect', () => {
+    console.log('Connected to Redis');
 });
 
 export default redisConnection;
